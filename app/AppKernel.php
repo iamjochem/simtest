b35dfba874c8fb891f125efe1089f7ebbd4c230a
Jochem Maas <jochem@iamjochem.com>
2015-03-10 14:45:16 +0100

    reinstate Twig bundle - without it Symfony cannot generate JSON responses from Exceptions thrown in controllers (the "_format" parameter of the route declaration is ignored)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -8,9 +8,6 @@ use Symfony\Component\Config\Loader\LoaderInterface;
  * used in this test-case application, under normal circumstances 
  * these commented out bundles would (probably/eventually) be used, 
  * therefore they have been left here as a reminder.
- *
- * NB: the Twig bundle is currently only loaded in "dev" and "test"
- *     environments because we only need it to render the _profiler!
  */
 
 class AppKernel extends Kernel
@@ -21,6 +18,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
@@ -30,7 +28,6 @@ class AppKernel extends Kernel
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Symfony\Bundle\TwigBundle\TwigBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
 //          $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
